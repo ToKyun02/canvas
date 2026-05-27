faa0f23b400233d9918d88a537c4733c75988520
ToKyun02 <developtk02@gmail.com>
2026-05-27 16:11:58 +0900

    chore : 안쓰는 util 함수 삭제

--- apps/web/src/utils/assignRef.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from 'react';
-
-export function assignRef<T>(ref: Ref<T> | undefined, value: T | null) {
-  if (typeof ref === 'function') {
-    ref(value);
-  } else if (ref) {
-    ref.current = value;
-  }
-}
